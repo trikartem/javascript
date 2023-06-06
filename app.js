1f40405162c1cad9c3d06caeb122d4ec1cbc646a
trikartem <trik.artem@gmail.com>
2023-06-06 14:38:15 +0300

    Расчёт проекта

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const city = 'Москва';
-const street = 'Новослободствая';
+const payRateUSD = 80;
+const AvailableHours = (11 - 2) * 5;
+const projectHours = 40;
+let cost;
 
-console.log(city + ', ' + street);
+console.log('Смогу ли выполнить работу ' + (projectHours <= AvailableHours));
+console.log('Стоимость работ: ' + payRateUSD * projectHours + '$');
