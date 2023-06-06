b67d70fb28178597628a72cbd166f9d126016bba
trikartem <trik.artem@gmail.com>
2023-06-06 15:11:25 +0300

    Шаблонные строки или литералы

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const payRateUSD = 80;
-const AvailableHours = (11 - 2) * 5;
-const projectHours = 40;
-let cost;
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий пупкин';
 
-console.log('Смогу ли выполнить работу ' + (projectHours <= AvailableHours));
-console.log('Стоимость работ: ' + payRateUSD * projectHours + '$');
+
+const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
+Ыconsole.log(template);
+
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2);
+
+const template3 = 'Проект \n' + 'Цена: ' + price;
+console.log(template3);
+
+const template4 = `Проект
+Цена: ${price}`;
+console.log(template4);
