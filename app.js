22861a43bbe11149ec40cb1e93ba7637cea74cca
trikartem <trik.artem@gmail.com>
2023-06-08 11:55:12 +0300

    Подсчёт депозита

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const projectName = 'Сайт магазина';
-const price = 2000;
-const author = 'Василий пупкин';
+const deposit = 12000;
+const rate = 0.07;
+const depositLength = 24;
+const result = deposit * (1 + rate / 12) ** depositLength;
+const houseCost = 13500;
 
-
-const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
-Ыconsole.log(template);
-
-const template2 = `${author} заказал ${projectName} по цене ${price}$`;
-console.log(template2);
-
-const template3 = 'Проект \n' + 'Цена: ' + price;
-console.log(template3);
-
-const template4 = `Проект
-Цена: ${price}`;
-console.log(template4);
+if (result > houseCost) {
+  console.log(`Мы накопили ${result}. Можем купить.Остаток ${result - houseCost}`);
+} else {
+  console.log(`Мы наколи: ${result}. Купить не сможем`);
+}
